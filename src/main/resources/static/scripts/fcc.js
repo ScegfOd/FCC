4a95030d8d75501e98072f3d9271047e4856806f
Marcus Kee <62085649+keemarcus@users.noreply.github.com>
2021-07-23 15:39:59 -0400

    added login verification and made a small change to sql script

--- src/main/resources/static/scripts/fcc.js
+++ src/main/resources/static/scripts/fcc.js
@@ -2,9 +2,14 @@ async function invalid() {
     let url = 'http://localhost:9000/invalid'
     let response = await fetch(url)
     let element = document.getElementById("invalid")
-    if (await response.json() == "invalid"){
+    let login_status = await response.json()
+    if (login_status == "invalid"){
         element.innerHTML += "Incorrect Login. Try Again."
-    }
+    } else if (login_status == "not manager") {
+		element.innerHTML += "Must be logged in as a manager<br>to access that page."
+	} else if (login_status == "not employee") {
+		element.innerHTML += "Must be logged in as an employee<br>to access that page."
+	}
 }
 
 window.onload = function () {
