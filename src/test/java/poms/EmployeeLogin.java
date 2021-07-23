48f22f919506b74544954f4a1a7477bef6ce7865
Jonathan Combs <JOfCombs@gmail.com>
2021-07-23 15:24:29 -0700

    cuke start

---
+++ src/test/java/poms/EmployeeLogin.java
@@ -0,0 +1,79 @@
+package poms;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EmployeeLogin {
+	@Given("That I am on the login page")
+	public void that_i_am_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter admin22 for my username and password1 as my password and press enter")
+	public void i_enter_admin22_for_my_username_and_password1_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify that the url of the page contains fccm.html")
+	public void i_verify_that_the_url_of_the_page_contains_fccm_html() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter admin87 for my username and password1 as my password and press enter")
+	public void i_enter_admin87_for_my_username_and_password1_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter admin22 for my username and oops as my password and press enter")
+	public void i_enter_admin22_for_my_username_and_oops_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify that the url of the page contains fcc.html")
+	public void i_verify_that_the_url_of_the_page_contains_fcc_html() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter empl13 for my username and password1 as my password and press enter")
+	public void i_enter_empl13_for_my_username_and_password1_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify that the url of the page contains fcce.html")
+	public void i_verify_that_the_url_of_the_page_contains_fcce_html() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter empl44 for my username and password1 as my password and press enter")
+	public void i_enter_empl44_for_my_username_and_password1_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter empl13 for my username and oops as my password and press enter")
+	public void i_enter_empl13_for_my_username_and_oops_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter dknight84@att.net for my username and password as my password and press enter")
+	public void i_enter_dknight84_att_net_for_my_username_and_password_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter dknight84@att.net for my username and oops as my password and press enter")
+	public void i_enter_dknight84_att_net_for_my_username_and_oops_as_my_password_and_press_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
